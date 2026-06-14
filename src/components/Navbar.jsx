7201b19fd437b3b36616228c08d194c5f31f2670
faisal-shohag <faisalshohagprog@gmail.com>
2026-06-14 20:14:52 +0600

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,24 @@
 "use client";
 
-import { Button } from "@heroui/react";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button, Dropdown, Label } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
+import { BiLogOut } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+import { MdDashboard } from "react-icons/md";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const { data: session } = authClient.useSession();
+  const user = session?.user;
+
+
+  const handleSignOut = async () => {
+    await authClient.signOut();
+  };
   return (
     <div>
       <div className="bg-black p-1 text-white">
@@ -76,12 +88,77 @@ const Navbar = () => {
               <Link href="/pricing">Pricing</Link>
             </li>
           </ul>
-          <div className="hidden items-center gap-4 md:flex">
-            <Link href="/signin">Login</Link>
-            <Link href="/signup">
-              <Button>Sign Up</Button>
-            </Link>
-          </div>
+         {!user && (
+            <div className="hidden items-center gap-4 md:flex">
+              <Link href="/signin">Login</Link>
+              <Link href="/signup">
+                <Button>Sign Up</Button>
+              </Link>
+            </div>
+          )}
+
+          {user && (
+            <div className="hidden items-center gap-4 md:flex">
+              <Dropdown>
+                <Dropdown.Trigger className="rounded-full">
+                  <Avatar size="sm" aria-label="Menu">
+                    <Avatar.Image
+                      referrerPolicy="no-referrer"
+                      alt="John Doe"
+                      src={user?.image}
+                    />
+                    <Avatar.Fallback>{user.name.charAt(0)}</Avatar.Fallback>
+                  </Avatar>
+                </Dropdown.Trigger>
+                <Dropdown.Popover>
+                  <div className="px-3 pt-3 pb-1">
+                    <div className="flex items-center gap-2">
+                      <Avatar size="sm">
+                        <Avatar.Image alt={user?.name} src={user?.image} />
+                        <Avatar.Fallback delayMs={600}>JD</Avatar.Fallback>
+                      </Avatar>
+                      <div className="flex flex-col gap-0">
+                        <p className="text-sm leading-5 font-medium">
+                          {user?.name}
+                        </p>
+                        <p className="text-xs leading-none text-muted">
+                          {user?.email}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <Dropdown.Menu
+                    onAction={(key) => console.log(`Selected: ${key}`)}
+                  >
+                    <Dropdown.Item id="new-file" textValue="New file">
+                      <Link
+                        className="flex items-center gap-2"
+                        href={`/dashboard/${user?.role}`}
+                      >
+                        <MdDashboard />
+                        <Label>Dashboard</Label>
+                      </Link>
+                    </Dropdown.Item>
+
+                    <Dropdown.Item id="copy-link" textValue="Copy link">
+                      <CgProfile />
+                      <Label>Profile</Label>
+                    </Dropdown.Item>
+
+                    <Dropdown.Item
+                      id="delete-file"
+                      textValue="Delete file"
+                      variant="danger"
+                      onClick={handleSignOut}
+                    >
+                      <BiLogOut />
+                      <Label>Logout</Label>
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown.Popover>
+              </Dropdown>
+            </div>
+          )}
         </header>
         {isMenuOpen && (
           <div className="border-t border-separator md:hidden">
